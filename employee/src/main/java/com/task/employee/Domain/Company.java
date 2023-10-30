64c203f2c014490099fdb2ca4725a7095439c471
Mridula Vats <mridula@mridula.vats>
2023-10-30 14:50:07 +0530

    practice second commit

--- employee/src/main/java/com/task/employee/Domain/Company.java
+++ employee/src/main/java/com/task/employee/Domain/Company.java
@@ -1,8 +1,11 @@
 package com.task.employee.Domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -16,5 +19,9 @@ public class Company {
 
     private String location;
     private String company_name;
+
+    @OneToMany(mappedBy = "company")
+    @JsonIgnore
+    private List<Employee> employeeList;
 
 }
